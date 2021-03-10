1e8baadc28cc1eb6608c3431b811ff83487359e2
ld432a <39087536+ld432a@users.noreply.github.com>
2021-03-10 14:11:41 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,2 +1,12 @@
 ##画图模板代码
 #
+import numpy as np
+import matplotlib.pyplot import plt
+
+# 曲线图
+x=np.range(0,100)
+plt.plot(X,y_predict,color="red",label="predict")
+plt.plot(X,y_label,color="blue",label="label")
+
+# 直方图
+plt.hist(train[:,i],bins=100)
